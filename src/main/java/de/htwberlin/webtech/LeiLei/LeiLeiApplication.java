9751d2745d3bc8dc8c7f9c7bdad84478c15f7d5f
Martin Leischner <martinleischner@web.de>
2021-10-20 13:23:18 +0200

    Added files

---
+++ src/main/java/de/htwberlin/webtech/LeiLei/LeiLeiApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.LeiLei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeiLeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeiLeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/LeiLei/LeiLeiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.LeiLei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeiLeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
